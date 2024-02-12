242f387ad03e7c4ecf9a8c8e148f0af4c593883b
DariaKhot <118306728+DariaKhot@users.noreply.github.com>
2024-02-11 19:36:44 -0500

    Create hugging_face_pipeline_test.py

---
+++ hugging_face_pipeline_test.py
@@ -0,0 +1,9 @@
+#Testing autotokenizer from hugging face
+
+from transformers import AutoTokenizer
+
+model_name = "nlptown/bert-base-multilingual-uncased-sentiment"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+encoding = tokenizer("We are very happy to show you the 🤗 Transformers library.")
+print(encoding)
